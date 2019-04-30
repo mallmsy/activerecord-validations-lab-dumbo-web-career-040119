08a57c3a9dbea81286f58e68ac328cefe683dccf
Mallory Woods <malvwoods@gmail.com>
2019-04-30 15:20:01 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates :phone_number, length: {minimum: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+
+  validates_presence_of :title
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+
+  validates :category, inclusion: {in:  %w(Fiction Non-Fiction)}
 end
